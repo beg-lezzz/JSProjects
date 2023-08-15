b47dba013fa25124c83b255a7572736d84433a95
malistov.artem <malistov.artem@russianpost.ru>
2023-08-15 22:20:34 +0300

    add final homework remarks: design, functions for buttons, popup

--- project003/script.js
+++ project003/script.js
@@ -21,24 +21,25 @@ const div = document.querySelector('.wrapper');
 let counter = 0;
 localUsers.forEach((user) => {
     const userCard = document.createElement('div');
+    userCard.classList.add('user-card');
     userCard.id = `${user.id}`;
+    const userCardInfo = document.createElement('div');
+    userCardInfo.classList.add('user-card-info');
+    const cardButtons = document.createElement('div');
+    cardButtons.classList.add('card-buttons');
+    userCard.append(userCardInfo);
+    userCard.append(cardButtons);
+
     for (const userKey in user) {
-        // console.log(userKey)
-        if (userKey !== 'id') {
-            if (typeof user[userKey] === 'object') {
-                userCard.insertAdjacentHTML('beforeend',
-                    `<p class="user-card">${userKey}: </p>`);
-                userCard.insertAdjacentHTML('beforeend',
-                    `<p class="user-card">${Object.values(user[userKey])}</p>`);
-            } else {
-                userCard.insertAdjacentHTML('beforeend',
-                    `<p class="user-card"> ${userKey}: ${user[userKey]}</p>`
-                )
-            }
+        if (userKey === 'name' || userKey === 'phone' || userKey === 'username') {
+            userCardInfo.insertAdjacentHTML('beforeend',
+                `<p class="user-info"> ${userKey}: ${user[userKey]}</p>`);
         }
     }
-    userCard.insertAdjacentHTML('beforeend',
-        `<button id="${user.id}" class="delete-button">Delete user</button>`)
+    cardButtons.insertAdjacentHTML('beforeend',
+        `<button id="${user.id}" class="delete-button card-button">Delete user</button>`);
+    cardButtons.insertAdjacentHTML('beforeend',
+        `<button id="${user.id}" class="info-button card-button">Show more</button>`);
     div.append(userCard);
 })
 
@@ -50,56 +51,62 @@ const rec = (arg) => {
     }
 }
 
-let buttonsArray = document.querySelectorAll('button');
-buttonsArray.forEach((btn) => {
+let buttonsDeleteArray = document.querySelectorAll('.delete-button');
+buttonsDeleteArray.forEach((btn) => {
     btn.addEventListener('click', (event) => {
-        // document.getElementById(btn.id).remove();
-        console.log(btn.parentElement.id);
-        btn.parentElement.remove();
-        const index = localUsers.findIndex(n => Number(n.id) === Number(btn.parentElement.id));
+        btn.closest('.user-card').remove();
+        const index = localUsers.findIndex(n => Number(n.id) === Number(btn.closest('.user-card').id));
         if (index !== -1) {
             localUsers.splice(index, 1);
         }
-        // console.log(localUsers);
         localStorage.setItem('users', JSON.stringify(localUsers));
-        // console.log(JSON.parse(localStorage.getItem('users')));
     })
 })
 
-// localUsers.forEach((user) => {
-//     getFiniteValue(user);
-// });
-//
-// function getFiniteValue(obj) {
-//     let handledFlag = 'temp__isAlreadyHandled__';
-//
-//     getProp(obj);
-//
-//     function getProp(o, stack) {
-//         var propertyPath;
-//
-//         for(var prop in o) {
-//             if(typeof(o[prop]) === 'object') {
-//                 if(!o[prop][handledFlag]) {
-//                     Object.defineProperty(o[prop],handledFlag, {
-//                         value: true,
-//                         writable:false,
-//                         configurable: true
-//                     });
-//
-//                     if(!stack)
-//                         propertyPath = 'rootObject.' + prop
-//                     else
-//                         propertyPath = stack + '.' + prop;
-//                     getProp(o[prop], propertyPath);
-//                 } else {
-//                     propertyPath = stack + '.' + prop;
-//                     console.error('Циклическая ссылка. Свойство: ' + propertyPath);
-//                 }
-//                 delete o[prop][handledFlag]
-//             } else {
-//                 console.log(`${prop}: ${o[prop]}`)
-//             }
-//         }
-//     }
-// }
+let buttonsInfoArray = document.querySelectorAll('.info-button');
+buttonsInfoArray.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        const index = localUsers.findIndex(n => Number(n.id) === Number(btn.closest('.user-card').id));
+        if (index === -1) {
+            return;
+        }
+        const popUp = document.createElement('div');
+        popUp.classList.add('popup');
+        const cross = document.createElement('div');
+        cross.textContent = "X";
+        cross.classList.add('cross');
+        popUp.append(cross);
+        const userFullInfo = document.createElement('div');
+        userFullInfo.classList.add('user-popup-info');
+        popUp.append(userFullInfo);
+        userFullInfo.insertAdjacentHTML('beforeend',
+            `<h2 class="full-user-info">Full user info</h2>`);
+        div.append(popUp);
+
+        const PopUpUser = localUsers[index];
+
+        for (const userKey in PopUpUser) {
+            if (userKey !== 'id') {
+                if (typeof PopUpUser[userKey] === 'object') {
+                    userFullInfo.insertAdjacentHTML('beforeend',
+                        `<p class="user-info">${userKey}: </p>`);
+                    userFullInfo.insertAdjacentHTML('beforeend',
+                        `<p class="user-info">${Object.values(PopUpUser[userKey])}</p>`);
+                } else {
+                    userFullInfo.insertAdjacentHTML('beforeend',
+                        `<p class="user-info"> ${userKey}: ${PopUpUser[userKey]}</p>`
+                    )
+                }
+            }
+        }
+        const closeCross = document.querySelector('.cross');
+        window.onscroll = () => {
+            window.scroll(0, 0);
+        };
+        closeCross.addEventListener('click', (event) => {
+            window.onscroll = () => {
+            };
+            closeCross.parentElement.remove();
+        })
+    })
+})
